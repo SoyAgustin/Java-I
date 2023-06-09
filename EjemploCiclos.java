3dca1e3fcd81a27098ce06628b90959145fac06f
Agustin <agustinphysx@gmail.com>
2023-06-09 13:35:53 -0600

    ciclos while,for y tablas de multiplicar

---
+++ EjemploCiclos.java
@@ -0,0 +1,17 @@
+
+public class EjemploCiclos {
+	public static void main(String[] args) {
+		System.out.println("Con ciclo For");
+		for(int i=0;i<11;i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("\n");
+		System.out.println("Con ciclo while");
+		int i=0;
+		while(i<11){
+			System.out.println(i);
+			i++;
+		}
+	}
+}
